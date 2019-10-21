72e26a619df2533e14f8f7225947723182692848
Teodor1998i <56401299+Teodor1998i@users.noreply.github.com>
2019-10-21 22:15:31 +0300

    Add files via upload

---
+++ Upr4_6/Upr4_6/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Upr4_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.largeRadioButton = new System.Windows.Forms.RadioButton();
+            this.mediumRadioButton = new System.Windows.Forms.RadioButton();
+            this.smallRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.thickRadioButton = new System.Windows.Forms.RadioButton();
+            this.thinRadioButton = new System.Windows.Forms.RadioButton();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.toppingCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.extraCheeseCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.largeRadioButton);
+            this.groupBox1.Controls.Add(this.mediumRadioButton);
+            this.groupBox1.Controls.Add(this.smallRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(150, 81);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size";
+            // 
+            // largeRadioButton
+            // 
+            this.largeRadioButton.AutoSize = true;
+            this.largeRadioButton.Location = new System.Drawing.Point(5, 58);
+            this.largeRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.largeRadioButton.Name = "largeRadioButton";
+            this.largeRadioButton.Size = new System.Drawing.Size(82, 17);
+            this.largeRadioButton.TabIndex = 2;
+            this.largeRadioButton.TabStop = true;
+            this.largeRadioButton.Text = "Large 13.75";
+            this.largeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mediumRadioButton
+            // 
+            this.mediumRadioButton.AutoSize = true;
+            this.mediumRadioButton.Location = new System.Drawing.Point(5, 38);
+            this.mediumRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.mediumRadioButton.Name = "mediumRadioButton";
+            this.mediumRadioButton.Size = new System.Drawing.Size(92, 17);
+            this.mediumRadioButton.TabIndex = 1;
+            this.mediumRadioButton.TabStop = true;
+            this.mediumRadioButton.Text = "Medium 11.50";
+            this.mediumRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // smallRadioButton
+            // 
+            this.smallRadioButton.AutoSize = true;
+            this.smallRadioButton.Location = new System.Drawing.Point(5, 18);
+            this.smallRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.smallRadioButton.Name = "smallRadioButton";
+            this.smallRadioButton.Size = new System.Drawing.Size(74, 17);
+            this.smallRadioButton.TabIndex = 0;
+            this.smallRadioButton.TabStop = true;
+            this.smallRadioButton.Text = "Small 9.25";
+            this.smallRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.thickRadioButton);
+            this.groupBox2.Controls.Add(this.thinRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 101);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(150, 67);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Crust";
+            // 
+            // thickRadioButton
+            // 
+            this.thickRadioButton.AutoSize = true;
+            this.thickRadioButton.Location = new System.Drawing.Point(5, 41);
+            this.thickRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.thickRadioButton.Name = "thickRadioButton";
+            this.thickRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.thickRadioButton.TabIndex = 1;
+            this.thickRadioButton.TabStop = true;
+            this.thickRadioButton.Text = "Thick";
+            this.thickRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // thinRadioButton
+            // 
+            this.thinRadioButton.AutoSize = true;
+            this.thinRadioButton.Location = new System.Drawing.Point(5, 18);
+            this.thinRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.thinRadioButton.Name = "thinRadioButton";
+            this.thinRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.thinRadioButton.TabIndex = 0;
+            this.thinRadioButton.TabStop = true;
+            this.thinRadioButton.Text = "Thin";
+            this.thinRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(182, 173);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(2);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(132, 19);
+            this.calculateButton.TabIndex = 13;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Toppings- a dollar for each";
+            // 
+            // infoRichTextBox
+            // 
+            this.infoRichTextBox.Location = new System.Drawing.Point(11, 206);
+            this.infoRichTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.infoRichTextBox.Name = "infoRichTextBox";
+            this.infoRichTextBox.ReadOnly = true;
+            this.infoRichTextBox.Size = new System.Drawing.Size(304, 154);
+            this.infoRichTextBox.TabIndex = 11;
+            this.infoRichTextBox.Text = "";
+            // 
+            // toppingCheckedListBox
+            // 
+            this.toppingCheckedListBox.FormattingEnabled = true;
+            this.toppingCheckedListBox.Location = new System.Drawing.Point(182, 40);
+            this.toppingCheckedListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.toppingCheckedListBox.Name = "toppingCheckedListBox";
+            this.toppingCheckedListBox.Size = new System.Drawing.Size(133, 124);
+            this.toppingCheckedListBox.TabIndex = 10;
+            // 
+            // extraCheeseCheckBox
+            // 
+            this.extraCheeseCheckBox.AutoSize = true;
+            this.extraCheeseCheckBox.Location = new System.Drawing.Point(17, 173);
+            this.extraCheeseCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.extraCheeseCheckBox.Name = "extraCheeseCheckBox";
+            this.extraCheeseCheckBox.Size = new System.Drawing.Size(112, 17);
+            this.extraCheeseCheckBox.TabIndex = 9;
+            this.extraCheeseCheckBox.Text = "Extra cheese 1.50";
+            this.extraCheeseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 379);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.infoRichTextBox);
+            this.Controls.Add(this.toppingCheckedListBox);
+            this.Controls.Add(this.extraCheeseCheckBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton largeRadioButton;
+        private System.Windows.Forms.RadioButton mediumRadioButton;
+        private System.Windows.Forms.RadioButton smallRadioButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton thickRadioButton;
+        private System.Windows.Forms.RadioButton thinRadioButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox infoRichTextBox;
+        private System.Windows.Forms.CheckedListBox toppingCheckedListBox;
+        private System.Windows.Forms.CheckBox extraCheeseCheckBox;
+    }
+}
+
