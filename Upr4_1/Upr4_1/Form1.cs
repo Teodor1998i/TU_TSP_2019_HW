3bf82390183c24d4eda7589577896bc3c11a9d37
Teodor1998i <56401299+Teodor1998i@users.noreply.github.com>
2019-10-21 21:19:58 +0300

    Add files via upload

---
+++ Upr4_1/Upr4_1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+
+        private void button_pal_Click(object sender, EventArgs e)
+        {
+            String str = textBox1.Text;
+            if (str != "")
+            {
+                
+                str = str.ToUpper();
+                char[] arr = str.ToCharArray();
+                Array.Reverse(arr);
+                string new_str = new string(arr);
+                int i = str.CompareTo(new_str);
+                if (i == 0)
+                {
+                    MessageBox.Show("Палиндром е");
+                }
+                else MessageBox.Show("Не е Палиндром!");
+            }
+            else {
+                    MessageBox.Show("Моля въведете нещо в полето!");
+                }
+            
+        }
+
+        private void button_ascii_Click(object sender, EventArgs e)
+        {
+            String str = textBox1.Text;
+            if (str != "")
+            {
+
+                String msg = "";
+                foreach (char val in str)
+                {
+                    msg += val.ToString() + " = " + ((int)val).ToString() + " \n";
+
+                }
+
+                MessageBox.Show(msg);
+            }
+            else {
+                MessageBox.Show("Моля въведете нещо в полето!");
+            }
+
+        }
+    }
+}
