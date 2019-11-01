285e39f32984071a105c1d6ee04f02179292b47c
Teodor1998i <56401299+Teodor1998i@users.noreply.github.com>
2019-11-01 22:38:35 +0200

    Add files via upload

---
+++ Upr5_1/Upr5_1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr5_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int month = int.Parse(dateTimePicker1.Value.ToString("MM"));
+            int day = int.Parse(dateTimePicker1.Value.ToString("dd"));
+            string astro_sign=""; 
+          
+
+            if (month == 12)
+            {                   
+                if (day < 22) 
+                astro_sign = "Strelec"; 
+                else
+                astro_sign ="Kozirog"; 
+            }  
+        
+            else if (month == 1)
+            { 
+                if (day < 20) 
+                astro_sign = "Kozirog"; 
+                else
+                astro_sign = "Vodolei"; 
+            }  
+        
+            else if (month == 2)
+            { 
+                if (day < 19) 
+                astro_sign = "Vodolei"; 
+                else
+                astro_sign = "Ribi"; 
+            } 
+              
+            else if(month == 3)
+            { 
+                if (day < 21)  
+                astro_sign = "Ribi"; 
+                else
+                astro_sign = "Oven"; 
+            } 
+
+            else if (month == 4)
+            { 
+                if (day < 20) 
+                astro_sign = "Oven"; 
+                else
+                astro_sign = "Telec"; 
+            } 
+              
+            else if (month == 5)
+            { 
+                if (day < 21) 
+                astro_sign = "Telec"; 
+                else
+                astro_sign = "Bliznaci"; 
+            } 
+              
+            else if( month == 6)
+            { 
+                if (day < 21) 
+                astro_sign = "Bliznaci"; 
+                else
+                astro_sign = "Rak"; 
+            } 
+              
+            else if (month == 7)
+            { 
+                if (day < 23) 
+                astro_sign = "Rak"; 
+                else
+                astro_sign = "luv"; 
+            } 
+              
+            else if( month == 8)
+            { 
+                if (day < 23)  
+                astro_sign = "Luv"; 
+                else
+                astro_sign = "Deva"; 
+            } 
+              
+            else if (month == 9)
+            { 
+                if (day < 23) 
+                astro_sign = "Deva"; 
+                else
+                astro_sign = "Vezni"; 
+            } 
+              
+            else if (month == 10)
+            { 
+                if (day < 23) 
+                astro_sign = "Vezni"; 
+                else
+                astro_sign = "Scorpion"; 
+            } 
+              
+            else if (month == 11)
+            { 
+                if (day < 22) 
+                astro_sign = "Scorpion"; 
+                else
+                astro_sign = "Strelec"; 
+            }
+
+            listBox1.Items.Add(astro_sign);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Upr5_2/Upr5_2/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr5_2
+
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public static bool add = false;
+        public static bool subtract = false;
+        public static bool multiply = false;
+        public static bool divide = true;
+        public static int counter = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {    
+
+            try
+            {
+                Timer timer1 = new Timer
+                {
+                    Interval = 1000
+                };
+
+                timer1.Enabled = true;
+                timer1.Tick += new EventHandler(timer1_Tick);
+               
+                Random rand = new Random();
+                int p;
+                var labels = new List<Label> { num1, num2, num3, num4, num5, num6, num7, num8 };
+
+                foreach (var label in labels)
+                {
+                    p = rand.Next(50) + 1;
+                    label.Text = p.ToString();
+                }
+
+            }
+            catch {
+                MessageBox.Show("Nope!");
+            }
+
+        }
+
+        public static void OnTimerEvent()
+        {
+            if (add && subtract && multiply && divide )
+            {
+                MessageBox.Show("Success!");
+            }
+
+            else {
+
+                MessageBox.Show("Wrong!");
+                
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+                int x1 = int.Parse(num1.Text);
+                int x2 = int.Parse(num2.Text);
+
+                int y1 = int.Parse(num3.Text);
+                int y2 = int.Parse(num4.Text);
+
+                int z1 = int.Parse(num5.Text);
+                int z2 = int.Parse(num6.Text);
+
+                double t1 = int.Parse(num7.Text);
+                double t2 = int.Parse(num8.Text);
+
+                int ans1 = int.Parse(a1.Text);
+                int ans2 = int.Parse(a2.Text);
+                int ans3 = int.Parse(a3.Text);
+                double ans4 = int.Parse(a4.Text); 
+
+                if (x1 + x2 == ans1)
+                {
+                    add = true;
+                }
+
+                if (y1 - y2 == ans2)
+                {
+                    subtract = true;
+                }
+
+                if (z1 * z2 == ans3)
+                {
+                    multiply = true;
+                }
+
+                double ans4_div = t1 / t2;
+                if (Math.Round(ans4_div, 2) == Math.Round(ans4, 2))
+                {
+                    divide = true;
+                }
+              
+                timer1.Enabled = false;
+                OnTimerEvent();
+            }
+
+            if (counter>=0) {
+                label_timer.Text = counter.ToString();
+            }
+            
+
+        }
+
+
+    }
+}
